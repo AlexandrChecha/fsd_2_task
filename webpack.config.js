bf610ad93ba47c54ba5c0700dd09aff367b3d471
AlexandrChecha <cv173@yandex.ru>
2020-06-19 01:16:32 +0300

    created main files, unsolved problem ERROR in ./scr/pages/style.scss 1:14

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 
 const PATHS = {
-	src: path.join(__dirname, 'scr'),
+	src: path.join(__dirname, 'scr', 'pages', 'UIkit'),
 	dist: path.join(__dirname, 'dist'),
 	assets: 'assets/',
 }
@@ -17,7 +17,7 @@ const PAGES_DIR = `${PATHS.src}`
 const PAGES = fs.readdirSync(PAGES_DIR).filter(fileName => fileName.endsWith('.pug'))
 
 module.exports = {
-	entry: './scr/index.js',
+	entry: './scr/pages/index.js',
 	output: {
 		path: path.resolve(__dirname, 'dist'),
 		filename: 'boundle.js'
@@ -32,41 +32,47 @@ module.exports = {
 	module: {
 		rules: [
 			{
-				test: /\.pug$/,
-				loaders: [
+				test: /\.scss$/,
+				include: [
+	                path.resolve(__dirname, 'node_modules'),
+					path.resolve(__dirname, 'scr', 'pages'), //<== This solved the issue
+            	],
+
+				use: [
 					{
-						loader: 'html-loader'
+						loader: MiniCssExtractPlugin.loader,
+						// options: {
+							
+						// },
 					},
 					{
+						loader: 'css-loader',
+						// options: {
+							
+						// },
+					},
+					{
+						loader: 'sass-loader',
+						// options: {
+
+						// },
+					},
+				],
+
+				test: /\.pug$/,
+				use: [
+					// {
+					// 	loader: 'html-loader'
+					// },
+				 	{
 						loader: 'pug-html-loader',
-						options: {
+						// options: {
             				
-          				},
+      //     				},
 					},
 					
 				],
 
-				test: /\.scss$/,
-				loaders: [
-					{
-						loader: MiniCssExtractPlugin.loader,
-						options: {
-							
-						},
-					},
-					{
-						loader: 'css-loader',
-						options: {
-							
-						},
-					},
-					{
-						loader: 'sass-loader',
-						options: {
-
-						},
-					},
-				],
 			},
 
 		],
@@ -78,14 +84,14 @@ module.exports = {
 		// 	filename: 'index.html',
 		// 	template: './scr/input.pug',
 		// 	minify: false,
-			new MiniCssExtractPlugin(),
-
-			...PAGES.map(page => new HtmlWebpackPlugin ({
-      		template: `${PAGES_DIR}/${page}`,
-      		filename: `./${page.replace(/\.pug/,'.html')}`
-
-			})),
 			
+		new MiniCssExtractPlugin(),
+
+		...PAGES.map(page => new HtmlWebpackPlugin ({
+      	template: `${PAGES_DIR}/${page}`,
+      	filename: `./${page.replace(/\.pug/,'.html')}`
+
+		})),
 			
 	],	
 
