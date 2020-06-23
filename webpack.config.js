c3c5239f9bf9a552a5cfc4d2fa83e8df532d6255
AlexandrChecha <cv173@yandex.ru>
2020-06-23 19:52:25 +0300

    automaticly generated config.js file -- work!

--- webpack.config.js
+++ webpack.config.js
@@ -1,98 +1,54 @@
-
-
+const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const fs = require('fs');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-
-
-const PATHS = {
-	src: path.join(__dirname, 'scr', 'pages', 'UIkit'),
-	dist: path.join(__dirname, 'dist'),
-	assets: 'assets/',
-}
-
-const PAGES_DIR = `${PATHS.src}`
-const PAGES = fs.readdirSync(PAGES_DIR).filter(fileName => fileName.endsWith('.pug'))
-
-module.exports = {
-	entry: './scr/pages/index.js',
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'boundle.js'
-
-	},
-
-	devServer: {
-		contentBase: path.join(__dirname, 'dist'),
-		port: 8081,
-	},	
-
-	module: {
-		rules: [
-			{
-				test: /\.scss$/,
-				include: [
-	                path.resolve(__dirname, 'node_modules'),
-					path.resolve(__dirname, 'scr', 'pages'), //<== This solved the issue
-            	],
-
-				use: [
-					{
-						loader: MiniCssExtractPlugin.loader,
-						// options: {
-							
-						// },
-					},
-					{
-						loader: 'css-loader',
-						// options: {
-							
-						// },
-					},
-					{
-						loader: 'sass-loader',
-						// options: {
-
-						// },
-					},
-				],
-
-				test: /\.pug$/,
-				use: [
-					// {
-					// 	loader: 'html-loader'
-					// },
-				 	{
-						loader: 'pug-html-loader',
-						// options: {
-            				
-      //     				},
-					},
-					
-				],
-
-			},
-
-		],
-	},
-
-
-	plugins: [
-		// new HtmlWebpackPlugin({
-		// 	filename: 'index.html',
-		// 	template: './scr/input.pug',
-		// 	minify: false,
-			
-		new MiniCssExtractPlugin(),
-
-		...PAGES.map(page => new HtmlWebpackPlugin ({
-      	template: `${PAGES_DIR}/${page}`,
-      	filename: `./${page.replace(/\.pug/,'.html')}`
-
-		})),
-			
-	],	
-
+const config = {
+  entry: './scr/pages/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      appMountId: 'app',
+      filename: 'index.html',
+      template: './scr/pages/colors_type.pug'
+    }),
+    new MiniCssExtractPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1
+            }
+          },
+          'postcss-loader'
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.pug$/,
+        use: [
+          'html-loader',
+          'pug-html-loader'
+        ]
+      }
+    ]
+  }
 };
+
+module.exports = config;
