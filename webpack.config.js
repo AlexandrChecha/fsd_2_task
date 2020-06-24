7af53e359267dd9f1a4d74c7a49f7cfef96ee651
AlexandrChecha <cv173@yandex.ru>
2020-06-24 22:04:38 +0300

    Fonts added to config.js, Quicksand font added to color&type.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,105 @@
-const webpack = require('webpack');
+
+
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const fs = require('fs');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const config = {
-  entry: './scr/pages/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      appMountId: 'app',
-      filename: 'index.html',
-      template: './scr/pages/colors_type.pug'
-    }),
-    new MiniCssExtractPlugin()
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 1
-            }
-          },
-          'postcss-loader'
-        ]
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'sass-loader'
-        ]
-      },
-      {
-        test: /\.pug$/,
-        use: [
-          'html-loader',
-          'pug-html-loader'
-        ]
-      }
-    ]
-  }
-};
 
-module.exports = config;
+
+const PATHS = {
+	src: path.join(__dirname, 'scr', 'pages'),
+	dist: path.join(__dirname, 'dist'),
+	assets: 'assets/',
+}
+
+const PAGES_DIR = `${PATHS.src}`
+const PAGES = fs.readdirSync(PAGES_DIR).filter(fileName => fileName.endsWith('.pug'))
+
+module.exports = {
+	entry: './scr/pages/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js'
+
+	},
+
+	devServer: {
+		contentBase: path.join(__dirname, 'dist'),
+		port: 8081,
+	},	
+
+	module: {
+		rules: [
+			{
+				test: /\.scss$/,
+				use: [
+					{
+						loader: MiniCssExtractPlugin.loader,
+						// options: {
+							
+						// },
+					},
+					{
+						loader: 'css-loader',
+						// options: {
+							
+						// },
+					},
+					{
+						loader: 'sass-loader',
+						// options: {
+
+						// },
+					},
+				],
+			},
+			{	
+				test: /\.pug$/,
+				use: [
+					{
+						loader: 'html-loader'
+					},
+				 	{
+						loader: 'pug-html-loader',
+						// options: {
+            				
+      //     				},
+					},
+					
+				],
+
+			},
+			{
+			    test: /\.(eot|svg|ttf|woff|woff2)$/,
+			    use: [
+			    		{
+			    			loader: 'file-loader',
+			    			options: {
+			    				name: './fonts/[name].[ext]',
+			    			},
+			    		}
+			         ],
+			},
+
+		],
+	},
+
+
+	plugins: [
+		// new HtmlWebpackPlugin({
+		// 	filename: 'index.html',
+		// 	template: './scr/input.pug',
+		// 	minify: false,
+			
+		new MiniCssExtractPlugin(),
+
+		...PAGES.map(page => new HtmlWebpackPlugin ({
+      	template: `${PAGES_DIR}/${page}`,
+      	filename: `./${page.replace(/\.pug/,'.html')}`
+
+		})),
+			
+	],	
+
+};
